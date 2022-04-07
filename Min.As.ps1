e1b271dd0f398c907f403ebde7ce1c5af69f3c34
Dimethylbiguanide Difrax <641503229@qq.com>
2022-04-08 00:11:50 +0800

    Min.AS

---
+++ Min.As.ps1
@@ -0,0 +1,58 @@
+﻿#Remove unnecessary characters from anglescripts
+#To disgust others
+Write-Output "Drop *.as files here."
+$filePath = Read-Host
+$filePath = $filePath.Trim("`"");
+if($filePath.ToLower().EndsWith(".as")){
+    $replaceMap = @{
+        " ," = ","; ", " = ",";
+        "+ " = "+"; " +" = "+";
+        "- " = "-"; " -" = "-";
+        " *" = "*"; "* " = "*";
+        "/ " = "/"; " /" = "/";
+        "; " = ";"; " ;" = ";";
+        " &" = "&"; "& " = "&";
+        " |" = "|"; "| " = "|";
+        " %" = "%"; "% " = "%";
+        " ^" = "^"; "^ " = "^";
+        " !" = "!";
+        "`t" = " ";
+        " ?" = "?"; "? " = "?";
+        " {" = "{"; "{ " = "{"; " }" = "}"; "} " = "}";
+        " (" = "("; "( " = "("; " )" = ")"; ") " = ")";
+        " <" = "<"; "< " = "<"; " >" = ">"; "> " = ">";
+        " [" = "["; "[ " = "["; " ]" = "]"; "] " = "]";
+        " =" = "="; "= " = "="
+    }
+    $aryScripts = @();
+    #此处换成按字符读取，处理/**/
+    Get-Content $filePath | ForEach-Object {
+        $szLine = $_.ToString().Trim()
+        if(!([string]::IsNullOrWhiteSpace($szLine)) -and !($szLine.StartsWith("//"))){
+            $oldc = ''
+            $newLine = ""
+            foreach($c in $szLine){
+                if(($c -eq '/') -and ($oldc -eq '/')){
+                    $newLine = $newLine.Substring(0, $newLine.Length - 1)
+                }
+                $oldc = $c
+                $newLine += $c
+            }
+            $aryScripts += @($newLine)
+        }
+    }
+    $out
+    foreach($szLine in $aryScripts){
+        foreach($k in $replaceMap.Keys){
+            if($szLine.Contains($k)){
+                $szLine = $szLine.Replace($k, $replaceMap[$k])
+            }
+        }
+        $szLine -replace " {2,}", " " | Out-File ($filePath.SubString(0, $filePath.Length - 3) + ".min.as") -NoNewline -Append utf8
+    }
+}
+else{
+    Write-Output "Invalid file..."
+}
+Write-Output "Any key exit"
+Read-Host | Out-Null
