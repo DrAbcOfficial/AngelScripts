8ec3a8d873ca8f35906ffb4a76c11e0d9dc26030
Dimethylbiguanide Difrax <Dr.Abc@cykaskin.com>
2022-04-10 00:32:06 +0800

    Update Min.As.ps1

--- Min.As.ps1
+++ Min.As.ps1
@@ -1,4 +1,4 @@
-﻿#Remove unnecessary characters from anglescripts
+#Remove unnecessary characters from anglescripts
 #To disgust others
 Write-Output "Drop *.as files here."
 $filePath = Read-Host
@@ -24,32 +24,66 @@ if($filePath.ToLower().EndsWith(".as")){
         " [" = "["; "[ " = "["; " ]" = "]"; "] " = "]";
         " =" = "="; "= " = "="
     }
-    $aryScripts = @();
-    #此处换成按字符读取，处理/**/
-    Get-Content $filePath | ForEach-Object {
-        $szLine = $_.ToString().Trim()
-        if(!([string]::IsNullOrWhiteSpace($szLine)) -and !($szLine.StartsWith("//"))){
-            $oldc = ''
-            $newLine = ""
-            foreach($c in $szLine){
-                if(($c -eq '/') -and ($oldc -eq '/')){
-                    $newLine = $newLine.Substring(0, $newLine.Length - 1)
-                }
-                $oldc = $c
-                $newLine += $c
+    $bInString = $false
+    $bInComment = $false
+    $bInMultiCommnet = $false
+    #now old older
+    $charBuffer = @("", "", "")
+    $buffer = ""
+    $content = Get-Content $filePath -Raw 
+    for($i = 0; $i -le $content.Length;$i++){
+        $charBuffer[2] = $charBuffer[1]
+        $charBuffer[1] = $charBuffer[0]
+        $charBuffer[0] = ([char]$content[$i]).ToString()
+        if($bInComment){
+            if($charBuffer[0] -eq "`n"){
+                $bInComment = $false
+                $charBuffer[0] = $charBuffer[1] = $charBuffer[2] = ""
             }
-            $aryScripts += @($newLine)
+            continue
+        }
+        if($bInMultiCommnet){
+            if(($charBuffer[0] -eq "/") -and ($charBuffer[1] -eq "*")){
+                $bInMultiCommnet = $false
+                $charBuffer[0] = $charBuffer[1] = $charBuffer[2] = ""
+            }
+            continue
+        }
+        
+        if(($charBuffer[0] -eq "`"") -and ($charBuffer[1] -ne "\")){
+            if($bInString){
+                $bInString = $false
+            }
+            else{
+                $bInString = $true
+            }
+        }
+        if($bInString -eq $false){
+            if(($charBuffer[0] -eq "/") -and ($charBuffer[1] -eq "/")){
+               $buffer += $charBuffer[2]
+               $bInComment = $true
+               continue
+            }
+            if(($charBuffer[0] -eq "*") -and ($charBuffer[1] -eq "/")){
+                $bInMultiCommnet = $true
+            }
+        }
+        if(($charBuffer[2] -ne "`n") -and ($charBuffer[2] -ne "`r")){
+            $buffer += $charBuffer[2]
         }
     }
-    $out
-    foreach($szLine in $aryScripts){
-        foreach($k in $replaceMap.Keys){
-            if($szLine.Contains($k)){
-                $szLine = $szLine.Replace($k, $replaceMap[$k])
-            }
-        }
-        $szLine -replace " {2,}", " " | Out-File ($filePath.SubString(0, $filePath.Length - 3) + ".min.as") -NoNewline -Append utf8
+    if(($charBuffer[1] -ne "`n") -and ($charBuffer[1] -ne "`r")){
+        $buffer += $charBuffer[1]
     }
+    if(($charBuffer[0] -ne "`n") -and ($charBuffer[0] -ne "`r")){
+        $buffer += $charBuffer[0]
+    }
+    foreach($k in $replaceMap.Keys){
+        if($buffer.Contains($k)){
+            $buffer = $buffer.Replace($k, $replaceMap[$k])
+        }
+    }
+    $buffer -replace " {2,}", " " | Out-File ($filePath.SubString(0, $filePath.Length - 3) + ".min.as") -Encoding utf8 -NoNewline
 }
 else{
     Write-Output "Invalid file..."
